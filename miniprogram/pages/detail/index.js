3ffcc897ba2c582c80a243c0979444cafdabfc44
xinag1 <492021770@qq.com>
2022-04-16 16:45:13 +0800

    1.4 优化物品详情页

--- miniprogram/pages/detail/index.js
+++ miniprogram/pages/detail/index.js
@@ -2,77 +2,90 @@
 const db = wx.cloud.database()
 const app = getApp();
 Page({
-  data: {
+    data: {
 
-  },
+    },
 
-  onLoad: function (options) {
-    this.setData({
-      list_id: options.list_id, // 保存上一页传来的 _id 字段
-      openid: app.globalData.openid,
-      UserInfo: app.globalData.UserInfo
-    })
-  },
-
-
-  onShow() {
-    this.showDetail()
-  },
-
-  // 根据 _id 值查询并显示数据
-  async showDetail(e) {
-    console.log(this.data.list_id.length)
-    if (this.data.list_id.length > 0) {
-      const db = await getApp().database()
-      // 根据 _id 值查询数据库中对应的待办事项
-      db.collection('products').where({
-        _id: this.data.list_id
-      }).get().then(res => {
-        // 解包获得待办事项
-        const {
-          data: [detail]
-        } = res
-        // 将数据保存到本地、更新显示
+    onLoad: function (options) {
         this.setData({
-          detail
+            list_id: options.list_id, // 保存上一页传来的 _id 字段
+            openid: app.globalData.openid,
+            UserInfo: app.globalData.UserInfo
         })
-      })
-    }
-  },
+    },
 
-  //借用响应
-  borrow(e) {
-    var that = this
-    wx.requestSubscribeMessage({
-      tmplIds: ['9tv28UFh_-9rl6P6w68_AWFSzi0nLQ9S3NgNy44tQog'],
-      success(res) {
-        that.addborrow()
-      },
-      fail(err) {
-        console.log(err)
-      }
-    })
-  },
-  addborrow() {
-    var that = this
-    db.collection("borrow").add({
-      data: {
-        userName: that.data.UserInfo.name,
-        openid: that.data.openid,
-        product: that.data.list_id,
-        borrowTime: Date.parse(new Date),
-        borrowRequest: 1,
-        borrow: 0,
-        returnRequest: 0,
-        return: 0
-      }
-    }).then(res => {
-      wx.showToast({
-        title: '已提交借用申请',
-      })
-    })
-    wx.switchTab({
-      url: '../list/index',
-    })
-  },
+
+    onShow() {
+        this.showDetail()
+    },
+
+    // 根据 _id 值查询并显示数据
+    async showDetail(e) {
+        console.log(this.data.list_id.length)
+        if (this.data.list_id.length > 0) {
+            const db = await getApp().database()
+            // 根据 _id 值查询数据库中对应的待办事项
+            db.collection('products').where({
+                _id: this.data.list_id
+            }).get().then(res => {
+                // 解包获得待办事项
+                const {
+                    data: [detail]
+                } = res
+                // 将数据保存到本地、更新显示
+                this.setData({
+                    detail
+                })
+            })
+        }
+    },
+
+    
+    //长按识别小程序码
+    previewImage: function (e) {
+        var url = e.target.dataset.src;
+        wx.previewImage({
+            current: url,
+            urls: [url]
+        })
+    },
+    
+    //借用响应
+    borrow(e) {
+        var that = this
+        wx.requestSubscribeMessage({
+            tmplIds: ['9tv28UFh_-9rl6P6w68_AWFSzi0nLQ9S3NgNy44tQog'],
+            success(res) {
+                that.addborrow()
+            },
+            fail(err) {
+                console.log(err)
+            }
+        })
+    },
+
+
+    //添加借用
+    addborrow() {
+        var that = this
+        db.collection("borrow").add({
+            data: {
+                userName: that.data.UserInfo.name,
+                openid: that.data.openid,
+                product: that.data.list_id,
+                borrowTime: Date.parse(new Date),
+                borrowRequest: 1,
+                borrow: 0,
+                returnRequest: 0,
+                return: 0
+            }
+        }).then(res => {
+            wx.showToast({
+                title: '已提交借用申请',
+            })
+        })
+        wx.switchTab({
+            url: '../list/index',
+        })
+    },
 })
