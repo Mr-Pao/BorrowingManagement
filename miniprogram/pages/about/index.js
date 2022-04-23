b2eee4fc29bbb1026207575050a289cfa06fd4b2
一只狍 <492021770@qq.com>
2022-04-23 14:35:10 +0800

    1.7 新增版本记录界面

--- miniprogram/pages/about/index.js
+++ miniprogram/pages/about/index.js
@@ -1,22 +1,28 @@
 // pages/about/index.js
 Page({
 
-    onShow() {
-    },
+  onShow() {},
 
-    //点击复制
-    copy: function (e) {
-        let item = e.currentTarget.dataset.item;
-        wx.setClipboardData({
-            data: item,
-            success(res) {
-                wx.showToast({
-                    title: '复制成功',
-                    icon: "success"
-                });
-            }
+  //跳转版本记录界面
+  version: function (e) {
+    wx.navigateTo({
+      url: '../version/index',
+    })
+  },
+
+  //点击复制
+  copy: function (e) {
+    let item = e.currentTarget.dataset.item;
+    wx.setClipboardData({
+      data: item,
+      success(res) {
+        wx.showToast({
+          title: '复制成功',
+          icon: "success"
         });
-    },
+      }
+    });
+  },
 
   //分享给好友
   onShareAppMessage: function () {
@@ -32,19 +38,19 @@ Page({
     }
   },
 
-    //跳转小程序
-    goToMiniProgram1: function () {
-        wx.navigateToMiniProgram({
-            appId: 'wx5d52c6c8f098ed5e', // 其他小程序APPID,必填
-            path: '', //其他小程序地址，非必填
-            success: res => {
-                // 打开成功
-                console.log("打开成功", res);
-            },
-            fail: err => {
-                console.log(err);
-            }
-        });
-    },
+  //跳转小程序
+  goToMiniProgram1: function () {
+    wx.navigateToMiniProgram({
+      appId: 'wx5d52c6c8f098ed5e', // 其他小程序APPID,必填
+      path: '', //其他小程序地址，非必填
+      success: res => {
+        // 打开成功
+        console.log("打开成功", res);
+      },
+      fail: err => {
+        console.log(err);
+      }
+    });
+  },
 
 })
